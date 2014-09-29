96753469ba20f7fa9e379fc57d08b1e5e88e45e3
Sterling Houghton <aussielb@gmail.com>
2014-09-28 19:00:15 -0700

    add relationship: has_many :listings

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
 
   validates :name, presence: true
-  
+
+  has_many :listings, dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
