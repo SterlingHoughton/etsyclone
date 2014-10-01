552eda944128641e10d7351bd7d042b1467e18d4
Sterling Houghton <aussielb@gmail.com>
2014-10-01 07:40:17 -0700

    add routes: sales - orders#sales, purchases - orders#purchases

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  
+
   resources :listings do
     resources :orders
   end
 
   devise_for :users
-  root 'listings#index'
-
 
   get 'pages/about'
   get 'pages/contact'
   get 'seller' => "listings#seller"
+  get 'sales' => "orders#sales"
+  get 'purchases' => "orders#purchases"
+
+  root 'listings#index'
 end
