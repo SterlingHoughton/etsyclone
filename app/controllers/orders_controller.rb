748b73592ee26ea31c8e4da8c776a2b3b37db912
Sterling Houghton <aussielb@gmail.com>
2014-10-02 09:52:26 -0700

    add code in controller to charge credit cards with stripe

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -25,8 +25,22 @@ class OrdersController < ApplicationController
     @order.buyer_id = current_user.id
     @order.seller_id = @seller.id
 
+    Stripe.api_key = ENV["STRIPE_API_KEY"]
+    token = params[:stripeToken]
+
+    begin
+      charge = Stripe::Charge.create(
+        :amount => (@listing.price * 100).floor,
+        :currency => "usd",
+        :card => token
+        )
+      flash[:notice] = "Thanks for ordering!"
+    rescue Stripe::CardError => e
+      flash[:danger] = e.message
+    end
+
     if @order.save
-      redirect_to root_url, notice: 'Order was successfully created.'
+      redirect_to root_url
     else
       render :new
     end
